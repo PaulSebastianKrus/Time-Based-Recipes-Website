5f570c06ae2500fca861d85fe505e789e0439356
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2025-02-27 03:15:30 +0100

    Commit all new changes

--- api/recipe/[id].js
+++ api/recipe/[id].js
@@ -1,30 +1,21 @@
-// api/recipe/[id].js
-const axios = require('axios');
+import axios from 'axios/dist/node/axios.cjs';
 
 const SPOONACULAR_API_KEY = '81a43d147d234da6a68bcc52f023b0e6';
 
-module.exports = async (req, res) => {
-  const recipeId = req.query.id;
-  
+export default async function handler(req, res) {
+  const { id } = req.query;
+
   try {
-    if (SPOONACULAR_API_KEY) {
-      const response = await axios.get(`https://api.spoonacular.com/recipes/${recipeId}/information`, {
-        params: {
-          apiKey: SPOONACULAR_API_KEY,
-          includeNutrition: true
-        }
-      });
-      
-      return res.json(response.data);
-    } else {
-      const recipe = sampleRecipes.find(r => r.id === parseInt(recipeId));
-      if (!recipe) {
-        return res.status(404).json({ error: 'Recipe not found' });
+    const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information`, {
+      params: {
+        apiKey: SPOONACULAR_API_KEY,
+        includeNutrition: true
       }
-      return res.json(recipe);
-    }
+    });
+
+    res.status(200).json(response.data);
   } catch (error) {
     console.error('Error fetching recipe details:', error);
     res.status(500).json({ error: 'Failed to fetch recipe details' });
   }
-};
+}
